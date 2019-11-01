a2745f1a5617b73ad9d7a5ab226a4f1bdbd5b311
Evan Plaice <evanplaice@gmail.com>
2019-11-01 15:25:48 -0600

    Add Implementation

---
+++ src/wc-monaco-editor.js
@@ -0,0 +1,73 @@
+import '../vendor/monaco/editor-main.bundle.js';
+
+self.MonacoEnvironment = {
+  getWorkerUrl: function (moduleId, label) {
+    if (label === 'json') {
+      return '../vendor/monaco/json.worker.bundle.js';
+    }
+    if (label === 'css') {
+      return '../vendor/monaco/css.worker.bundle.js';
+    }
+    if (label === 'html') {
+      return '../vendor/monaco/html.worker.bundle.js';
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return '../vendor/monaco/ts.worker.bundle.js';
+    }
+    return '../vendor/monaco/editor.worker.bundle.js';
+  }
+}
+
+export class WCMonacoEditor extends HTMLElement {
+  __element = null;
+  __editor = null;
+
+  static get observedAttributes() {
+    return ['src', 'value'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this[name] = newValue;
+    }
+  }
+
+  get src() { return this.getAttribute('src'); }
+  set src(value) {
+    this.setAttribute('src', value);
+    this.fetchSrc();
+  }
+
+  get value() { return this.__editor.getValue(); }
+  set value(value) {
+    this.__editor.setValue(value);
+  }
+
+  async connectedCallback() {
+    this.initialize();
+
+    if (this.hasAttribute('src')) {
+      this.fetchSrc();
+    }
+  }
+  
+  initialize() {
+    this.__element = document.createElement('div');
+    this.__element.id = this.hasAttribute('id') ? `${this.getAttribute('id')}-editor` : 'editor';
+    this.__element.style = this.style.cssText;
+    this.appendChild(this.__element);
+    this.__editor = monaco.editor.create(document.getElementById(this.__element.id), {
+      language: this.getAttribute('language'),
+      theme: 'vs-dark'
+    });
+  }
+
+  async fetchSrc() {
+    // fetch the external markdown source
+    const response = await fetch(this.src);
+    const contents = await response.text();
+    this.__editor.setValue(contents);
+  }
+}
+
+customElements.define('wc-monaco-editor', WCMonacoEditor);
