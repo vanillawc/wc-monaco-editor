8fa171c343651e73ab9b94811947cff85419ba60
Evan Plaice <evanplaice@gmail.com>
2019-11-27 19:50:54 -0700

    Lint everything

--- src/wc-monaco-editor.js
+++ src/wc-monaco-editor.js
@@ -1,5 +1,7 @@
+/* eslint no-undef: 0 */
 import '../vendor/monaco/editor-main.bundle.js';
 
+// eslint-disable-next-line
 self.MonacoEnvironment = {
   getWorkerUrl: function (moduleId, label) {
     if (label === 'json') {
@@ -16,51 +18,50 @@ self.MonacoEnvironment = {
     }
     return '../vendor/monaco/editor.worker.bundle.js';
   }
-}
+};
 
 export class WCMonacoEditor extends HTMLElement {
-
-  constructor() {
+  constructor () {
     super();
     this.__element = null;
     this.__editor = null;
   }
 
-  static get observedAttributes() {
+  static get observedAttributes () {
     return ['src', 'value'];
   }
 
-  attributeChangedCallback(name, oldValue, newValue) {
+  attributeChangedCallback (name, oldValue, newValue) {
     if (oldValue !== newValue) {
       this[name] = newValue;
     }
   }
 
-  get src() { return this.getAttribute('src'); }
-  set src(value) {
+  get src () { return this.getAttribute('src'); }
+  set src (value) {
     this.setAttribute('src', value);
     this.fetchSrc();
   }
 
-  get value() { return this.__editor.getValue(); }
-  set value(value) {
+  get value () { return this.__editor.getValue(); }
+  set value (value) {
     this.__editor.setValue(value);
   }
 
-  async connectedCallback() {
+  async connectedCallback () {
     this.initialize();
 
     if (this.hasAttribute('src')) {
       this.fetchSrc();
     }
   }
-  
-  initialize() {
+
+  initialize () {
     // set a unique id (defaults to 'editor')
     this.__element = document.createElement('div');
     this.__element.id = this.hasAttribute('id') ? `${this.getAttribute('id')}-editor` : 'editor';
     // set styling (defaults to fit outer container)
-    this.__element.style = this.hasAttribute('style') ? this.style.cssText: 'width:100%;height:100%';
+    this.__element.style = this.hasAttribute('style') ? this.style.cssText : 'width:100%;height:100%';
     this.appendChild(this.__element);
 
     // create the editor
@@ -70,7 +71,7 @@ export class WCMonacoEditor extends HTMLElement {
     });
   }
 
-  async fetchSrc() {
+  async fetchSrc () {
     // fetch the external markdown source
     const response = await fetch(this.src);
     const contents = await response.text();
