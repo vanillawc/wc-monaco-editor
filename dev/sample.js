c62e6697027e9cb1a140d09cd6a772c6ff14646e
Evan Plaice <evanplaice@gmail.com>
2019-12-21 23:05:23 -0700

    Refactor

--- dev/sample.js
+++ dev/sample.js
@@ -1,3 +1,3 @@
-function x() {
+function x () {
   console.log("Hello world!");
 }
--- src/wc-monaco-editor.js
+++ src/wc-monaco-editor.js
@@ -21,17 +21,12 @@ self.MonacoEnvironment = {
 };
 
 export class WCMonacoEditor extends HTMLElement {
-  constructor () {
-    super();
-    this.__element = null;
-    this.__editor = null;
-  }
-
   static get observedAttributes () {
     return ['src', 'value'];
   }
 
   attributeChangedCallback (name, oldValue, newValue) {
+    if (!this.__initialized) { return; }
     if (oldValue !== newValue) {
       this[name] = newValue;
     }
@@ -40,7 +35,7 @@ export class WCMonacoEditor extends HTMLElement {
   get src () { return this.getAttribute('src'); }
   set src (value) {
     this.setAttribute('src', value);
-    this.fetchSrc();
+    this.setSrc();
   }
 
   get value () { return this.__editor.getValue(); }
@@ -48,34 +43,39 @@ export class WCMonacoEditor extends HTMLElement {
     this.__editor.setValue(value);
   }
 
-  async connectedCallback () {
-    this.initialize();
-
-    if (this.hasAttribute('src')) {
-      this.fetchSrc();
-    }
-  }
-
-  initialize () {
-    // set a unique id (defaults to 'editor')
+  constructor () {
+    super();
+    this.__initialized = false;
     this.__element = document.createElement('div');
-    this.__element.id = this.hasAttribute('id') ? `${this.getAttribute('id')}-editor` : 'editor';
-    // set styling (defaults to fit outer container)
-    this.__element.style = this.hasAttribute('style') ? this.style.cssText : 'width:100%;height:100%';
+    this.__element.id = this.hasAttribute('id')
+      ? `${this.getAttribute('id')}-editor`
+      : 'editor';
+    this.__element.style = this.hasAttribute('style')
+      ? this.style.cssText
+      : 'width:100%;height:100%';
     this.appendChild(this.__element);
-
-    // create the editor
     this.__editor = monaco.editor.create(document.getElementById(this.__element.id), {
       language: this.getAttribute('language'),
       theme: 'vs-dark'
     });
   }
 
-  async fetchSrc () {
-    // fetch the external markdown source
-    const response = await fetch(this.src);
-    const contents = await response.text();
+  async connectedCallback () {
+    if (this.hasAttribute('src')) {
+      this.setSrc();
+    }
+    this.__initialized = true;
+  }
+
+  async setSrc () {
+    const src = this.getAttribute('src');
+    const contents = await this.fetchSrc(src);
     this.__editor.setValue(contents);
+  }
+
+  async fetchSrc (src) {
+    const response = await fetch(src);
+    return response.text();
   }
 }
 
